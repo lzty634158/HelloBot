a6cb8b28984e2f76386fd2aaa6fb8b2c26e51fff
lzty634158 <lzty634158@github.com>
2019-03-07 20:29:23 +0800

    sort ultronsonic

--- main.ts
+++ main.ts
@@ -610,17 +610,22 @@ namespace HelloBot {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Ultrasonic_Car(): number {
 
-        // send pulse
-        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
-        pins.digitalWritePin(DigitalPin.P14, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(DigitalPin.P14, 1);
-        control.waitMicros(15);
-        pins.digitalWritePin(DigitalPin.P14, 0);
-
-        // read pulse
-        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
-        return  Math.floor(d / 58);
+        // send pulse       
+        let list:Array<number> = [0, 0, 0, 0, 0];
+        for (var i = 0; i < 5; i++) {
+            pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+		        pins.digitalWritePin(DigitalPin.P14, 0);
+		        control.waitMicros(2);
+		        pins.digitalWritePin(DigitalPin.P14, 1);
+		        control.waitMicros(15);
+		        pins.digitalWritePin(DigitalPin.P14, 0);
+		
+		        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
+		        list[i] = Math.floor(d / 58)
+        }
+        list.sort();
+        let length = (list[1] + list[2] + list[3])/3;
+        return  length;
     }
 
     //% blockId=HelloBot_Avoid_Sensor block="Avoid_Sensor|direct %direct|value %value"
